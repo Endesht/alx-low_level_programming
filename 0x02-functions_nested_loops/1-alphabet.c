0706c210c97065166ec30f06b0fc94455c42c1b6
endesht <endesht@outlook.com>
2023-09-14 08:52:09 -0700

    sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Main - Print alphabet lower case
+ *
+ * Return:void
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
