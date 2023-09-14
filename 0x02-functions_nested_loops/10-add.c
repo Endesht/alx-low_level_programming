96c825fc069aa7ce95ed669b6111d935e8945d56
endesht <endesht@outlook.com>
2023-09-14 10:30:13 -0700

    add integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1: first number computed
+ * @2n: second number computed
+ * Reurn: Integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
